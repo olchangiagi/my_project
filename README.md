4c85f0f5f169f62868e6b6a1b20f1463953fd07a
olchangiagi <olchangiagi@gmail.com>
2026-07-22 14:44:22 +0900

    Feat: git action checkout까지 진행

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ git 사용 연
 ```
     uvicorn main:app --reload
 ```
+
+# 구조
+my_project/
+L main.py
+L templates/        <-- html 위치
+    L index.html
+L static/           <-- 정적 데이터 위치: css, js, 리소스
+    L assets
+        L *.jpg|png
+    L css
+        L *.css
+    L js
+        L *.js
--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 # 1. 모듈 가져오기
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles # 정적 데이터 처리 모듈
 
 # 2. Fastapi 객체 생성, 전역변수 생성
 app = FastAPI()
-templates = Jinja2Templates(directory = "templates")
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static") # 정적데이터 경로설정
 
 # 3. 라우팅 구성/정의
 @app.get("/")
 def home(req:Request):
-    # 홈페이지("/") get 방식 요청 -> 매칭되는 함수 home() 호출됨 
-    # -> index.html을 읽어서 req데이터를 전달하여 동적 html 구성
-    # -> 응답(return) -> 클라이언트 브라우저에게 전달 -> 랜더링, Dom tree
-    # -> 브라우저 해석후 화면애 그리기 -> 클라이언트는 응답된 결과를 화면에서 볼 수 있음
-    return templates.TemplateResponse("index.html", {"request":req})
+    # 홈페이지("/") get방식 요청 -> 매칭되는 함수 home() 호출됨
+    # -> index.html을 읽어서 req 데이터를 전달하여 동적 html 구성 
+    # -> 응답 (return) -> 클라이언트 브라우저에게 전달 -> 랜더링, Dom tree
+    # -> 브라우저 해석 화면에 그리기 -> 클라이언트는 응답결과를 화면에서 볼수있다
+    return templates.TemplateResponse(req, "index.html")
